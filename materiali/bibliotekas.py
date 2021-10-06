92bdfdd1b4df4c65729fe8ef9fc1588edcfc4f3d
Daniils <76158687+Spector255@users.noreply.github.com>
2021-10-06 12:07:05 +0000

    Update

--- materiali/bibliotekas.py
+++ materiali/bibliotekas.py
@@ -3,15 +3,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = list(range(0,11))
+x = list(range(-10,11))
 y = [i**2 for i in x]
-# Data for plotting
 
 fig, ax = plt.subplots()
-ax.plot(x, y)
+ax.plot(x, y, color = "green", linewidth = "5")
 
-ax.set(xlabel='X', ylabel='X**2',
-       title='X**2')
+#ax.set(xlabel = 'X', ylabel = 'X**2', title = 'X**2')
+plt.title("X**2")
 ax.grid()
 
 #fig.savefig("test.png")
